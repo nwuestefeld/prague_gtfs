81ba60548c5c560bcea56363c4f22c45bd55de38
nils.wuestefeld <nils.wuestefeld@stud.uni-goettingen.de>
2025-05-08 21:32:13 +0200

    first steps toward login page + prep for database stuff + placeholder for todos

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -2,3 +2,11 @@ import streamlit as st
 
 st.title("Dashboard")
 st.write("This is a Dashboard page.")
+
+##fetch database stuff
+st.subheader("Delay Statistics")
+
+#filter dataframe
+
+#plot top 10 delays
+st.subheader("Top 10 Delays")
